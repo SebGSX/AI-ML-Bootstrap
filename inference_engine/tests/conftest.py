3d5beccf14b4039513c08f23710a30624d15fbd2
Seb Garrioch <seb_g@live.com>
2024-10-24 21:52:02 +0100

    Removed superfluous test support code.

--- inference_engine/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-# © 2023 Seb Garrioch. All rights reserved.
-# Published under the MIT License.
-import os
-import sys
-
-
-_src_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/'))
-sys.path.insert(0, _src_directory)
